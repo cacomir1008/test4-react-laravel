d63d3eb8a503f52832e61f324f7a1c185d047570
tsubasa-0512 <wasedatennis0512@gmail.com>
2021-04-29 15:44:41 +0900

    ssl化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
